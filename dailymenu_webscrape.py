bf3196ae5188cd29cf83819934ee374c278f6153
DajanaKolarova <dajana.kolarova@gmail.com>
2025-05-20 22:24:19 +0200

    fix

--- dailymenu_webscrape.py
+++ dailymenu_webscrape.py
@@ -1,6 +1,6 @@
 import requests
 import logging
-from .models import JidelnaCVUT, JidelnaCVUTDenniMenu # asdf
+from .models import JidelnaCVUT, JidelnaCVUTDenniMenu # fix this
 from celery import shared_task
 
 # Configure logging
