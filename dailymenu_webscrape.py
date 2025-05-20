492b1c0efb40840a5766e4d38ef6b92e1d7e2a59
DajanaKolarova <dajana.kolarova@gmail.com>
2025-05-20 22:51:08 +0200

    idea

--- dailymenu_webscrape.py
+++ dailymenu_webscrape.py
@@ -1,23 +1,23 @@
 import requests
 import logging
-from .models import JidelnaCVUT, JidelnaCVUTDenniMenu # fix this
-from celery import shared_task
 
 # Configure logging
 logger = logging.getLogger(__name__)
 logging.basicConfig(level=logging.INFO)
 
 
-@shared_task
 def scrape_and_save_denni_menu():
     urls = {
-        "menza_studentskyDum": "https://agata.suz.cvut.cz/jidelnicky/index.php?clPodsystem=2&lang=cs",
-        "technicka_menza": "https://agata.suz.cvut.cz/jidelnicky/index.php?clPodsystem=3&lang=cs",
+    
+        "technicka_menza": "https://agata.suz.cvut.cz/jidelnicky/indexTyden.php?clPodsystem=3&lang=cs",
+        "restaurant_kulatak": "https://kulatak.cz/#menu-obsah", # pomocí beautiful soup najít si v web inspector paragrafy <p jednotlivých jídel který vyprintuješ
+        "restaurant_utopolu": "https://www.utopolu.cz/menu", #je to obrázek takže musím použít OCR pytesseract 
+        #cafe organica potřebuju login přes selenium nebo cookies
     }
 
     try:
         # Create or update a single entry for the daily menu
-        denni_menu, created = JidelnaCVUTDenniMenu.objects.get_or_create()
+       
 
         for jidelna, url in urls.items():
             try:
@@ -35,25 +35,6 @@ def scrape_and_save_denni_menu():
             except requests.RequestException as e:
                 logger.error(f"Failed to scrape {jidelna}: {e}")
 
-        # Save all changes to the database
-        denni_menu.save()
-        logger.info("Successfully saved the daily menu.")
-
     except Exception as e:
         logger.error(f"Unexpected error: {e}")
 
-
-@shared_task
-def scrape_jidelna_cvut():
-    url = "https://agata.suz.cvut.cz/jidelnicky/indexTyden.php?clPodsystem=1&lang=cs"
-    try:
-        response = requests.get(url)
-        response.raise_for_status()  # Raise an error if the request failed
-        page_html = response.text
-
-        # Save the HTML to the JidelnaCVUT model
-        JidelnaCVUT.objects.create(page_html=page_html)
-        logger.info("Successfully scraped and saved Jidelna CVUT page.")
-    except requests.RequestException as e:
-        # Log the error or handle it as needed
-        logger.error(f"Failed to scrape JidelnaCVUT: {e}")
